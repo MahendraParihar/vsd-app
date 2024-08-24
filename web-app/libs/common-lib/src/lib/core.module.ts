85e2b4a9c27286fbdc36a5fdae8edd5c4bd69562
mahendrakelp <mahendra.p@kelpglobal.com>
2024-08-24 22:53:40 +0530

    footer

---
+++ web-app/libs/common-lib/src/lib/core.module.ts
@@ -0,0 +1,24 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LabelService } from './label/label.service';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpService, SnackBarService, StorageService } from './services';
+
+export function appLabelInitialize(appLabelService: LabelService) {
+  return (): Promise<any> => {
+    return appLabelService.loadLabels();
+  };
+}
+
+@NgModule({
+  imports: [CommonModule, MatSnackBarModule],
+  providers: [
+    LabelService,
+    SnackBarService,
+    HttpService,
+    StorageService,
+    { provide: APP_INITIALIZER, useFactory: appLabelInitialize, deps: [LabelService], multi: true }
+  ],
+  exports: [],
+})
+export class CoreModule {}
