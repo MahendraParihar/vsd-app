6504f4f077fa1119928f5415e947654d5c8c65f4
mahendrakelp <mahendra.p@kelpglobal.com>
2025-02-11 22:56:34 +0530

    docker added

--- server/libs/common/src/lib/health/health.controller.ts
+++ server/libs/common/src/lib/health/health.controller.ts
@@ -1,10 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService, SequelizeHealthIndicator } from '@nestjs/terminus';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  SequelizeHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
+    private readonly disk: DiskHealthIndicator,
+    private memory: MemoryHealthIndicator,
     private db: SequelizeHealthIndicator,
   ) {
   }
@@ -12,7 +20,12 @@ export class HealthController {
   @Get()
   @HealthCheck()
   check() {
-    const checks = [() => this.db.pingCheck('database', { timeout: 5000 })];
+    // RSS is the Resident Set Size and is used to show how much memory is allocated to that process and is in RAM. It does not include memory that is swapped out. It does include memory from shared libraries as long as the pages from those libraries are actually in memory. It does include all stack and heap memory.
+    const checks = [
+      () => this.db.pingCheck('database', { timeout: 5000 }),
+      () => this.disk.checkStorage('storage', { path: '/', threshold: 50 * 1024 * 1024 * 1024 }),
+      () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
+    ];
     return this.health.check(checks);
   }
 }
