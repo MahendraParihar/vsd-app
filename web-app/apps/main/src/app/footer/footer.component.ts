1b16844fd4fb61a0b7ccbdad8bbd82063f6ea000
mahendrakelp <mahendra.p@kelpglobal.com>
2025-03-02 22:57:28 +0530

    feat : banner height

--- web-app/apps/main/src/app/footer/footer.component.ts
+++ web-app/apps/main/src/app/footer/footer.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { LabelService } from '@core-lib';
-import { LabelKey } from '@vsd-common/lib';
+import { IMandalDetail, LabelKey } from '@vsd-common/lib';
+import { Meta, MetaDefinition } from '@angular/platform-browser';
+import { MandalService } from '../mandal/services/mandal.service';
 
 @Component({
   selector: 'vsd-web-app-footer',
@@ -9,12 +11,15 @@ import { LabelKey } from '@vsd-common/lib';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   protected readonly LabelKey = LabelKey;
   btnList: { label: string, path: string, isActive: boolean }[] = [];
+  primaryMandal!: IMandalDetail;
 
   constructor(private router: Router,
-              protected labelService: LabelService) {
+              protected labelService: LabelService,
+              private mandalService: MandalService,
+              private metaService: Meta) {
     this.btnList = [
       { label: this.labelService.getLabel(LabelKey.SIDE_MENU_EVENT), path: 'event', isActive: false },
       { label: this.labelService.getLabel(LabelKey.SIDE_MENU_TEMPLE), path: 'temple', isActive: false },
@@ -25,10 +30,37 @@ export class FooterComponent {
       { label: this.labelService.getLabel(LabelKey.SIDE_MENU_CONTACT_US), path: 'contact-us', isActive: false },
     ];
     this.router.events.subscribe((params) => {
-      if (params instanceof NavigationEnd){
-        this.setActiveState()
+      if (params instanceof NavigationEnd) {
+        this.setActiveState();
       }
     });
+  }
+
+  async ngOnInit() {
+    await this.loadPrimaryMandal();
+  }
+
+  async loadPrimaryMandal() {
+    this.primaryMandal = await this.mandalService.loadPrimaryMandalDetails();
+    this.bindSEOData();
+  }
+
+  bindSEOData() {
+    if (!this.primaryMandal) {
+      return;
+    }
+    const seoArray: MetaDefinition[] = [];
+    if (this.primaryMandal.tags) {
+      seoArray.push({ name: 'keyword', content: this.primaryMandal.tags.join(',') });
+    }
+    if (this.primaryMandal.metaDescription) {
+      seoArray.push({ name: 'description', content: this.primaryMandal.metaDescription });
+    }
+    this.metaService.addTags(seoArray);
+  }
+
+  openLink(link: string) {
+    window.open(link, '_blank');
   }
 
   async onClick(btn: { label: string; path: string; isActive: boolean }) {
