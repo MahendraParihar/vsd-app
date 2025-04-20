62ca6c567190148f54c724ce75f794d2dcccaf88
mahendrakelp <mahendra.p@kelpglobal.com>
2025-04-20 07:58:21 +0530

    feat : Banners

--- web-app/apps/main/src/app/home/home.component.ts
+++ web-app/apps/main/src/app/home/home.component.ts
@@ -1,6 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EventService } from '../event/services/event.service';
-import { IBannerList, IEventDetail, IFacilityList, ILegalPageList, ITempleList, LabelKey, MediaForEnum } from '@vsd-common/lib';
+import {
+  IBannerList,
+  IEventDetail,
+  IFacilityList,
+  ILegalPageList,
+  ITempleList,
+  LabelKey,
+  MediaForEnum,
+} from '@vsd-common/lib';
 import { BannerService, LabelService } from '@core-lib';
 import { TempleService } from '../temple/services/temple.service';
 import { CommonService } from '../common.service';
@@ -15,12 +23,14 @@ import { FacilityService } from '../facility/services/facility.service';
 })
 export class HomeComponent implements OnInit {
   bannerService = inject(BannerService);
+  labelService = inject(LabelService);
+
   banners: IBannerList[] = [];
-  templeTitle = 'मंदिर';
-  eventTitle = 'आगामी कार्यक्रम';
-  aboutUsTitle = 'हमारे बारे में';
-  trusteeTitle = 'हमारे ट्रस्टी';
-  facilitiesTitle = 'सुविधाएँ';
+  templeTitle = this.labelService.getLabel(LabelKey.HOME_TEMPLE_TITLE);
+  eventTitle = this.labelService.getLabel(LabelKey.HOME_EVENT_TITLE);
+  aboutUsTitle = this.labelService.getLabel(LabelKey.HOME_ABOUT_US_TITLE);
+  trusteeTitle = this.labelService.getLabel(LabelKey.HOME_TRUSTEE_TITLE);
+  facilitiesTitle = this.labelService.getLabel(LabelKey.HOME_FACILITY_TITLE);
   protected readonly LabelKey = LabelKey;
 
   upcomingEvents: IEventDetail[] = [];
@@ -31,30 +41,26 @@ export class HomeComponent implements OnInit {
   constructor(private eventService: EventService,
               private templeService: TempleService,
               private facilityService: FacilityService,
-              protected labelService: LabelService,
               private commonService: CommonService,
               private route: Router) {
   }
 
   async ngOnInit() {
     this.banners = await this.bannerService.loadBanner(MediaForEnum.HOME);
-    this.facilitiesTitle = this.labelService.getLabel(this.LabelKey.SIDE_MENU_FACILITY);
-    this.aboutUsTitle = this.labelService.getLabel(this.LabelKey.SIDE_MENU_ABOUT_US);
-    this.templeTitle = this.labelService.getLabel(this.LabelKey.SIDE_MENU_TEMPLE);
     await this.loadDetails();
   }
 
   async loadDetails() {
-    const res = await Promise.all([
+    const [events, temples, facilities, legalPage] = await Promise.all([
       await this.eventService.loadUpcomingEvents(),
       this.templeService.loadHomeTemples(),
       this.facilityService.loadHomeFacilities(),
       this.commonService.loadPage('about-us'),
     ]);
-    this.upcomingEvents = res[0];
-    this.temples = res[1];
-    this.facilities = res[2];
-    this.legalPage = res[3];
+    this.upcomingEvents = events;
+    this.temples = temples;
+    this.facilities = facilities;
+    this.legalPage = legalPage;
   }
 
   aboutUsClick() {
